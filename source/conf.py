ed9af1ada1629b3018e06b17a8d0765080418d6f
enzo <enzo.ccc@gmail.com>
2022-07-05 08:26:59 +0200

    sarted add api doc

--- source/conf.py
+++ source/conf.py
@@ -95,7 +95,7 @@ html_logo = '_static/pyarchinit_logo.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_css_files = ['style.css',]
 # -- Options for LaTeX output ---------------------------------------------
 
 
